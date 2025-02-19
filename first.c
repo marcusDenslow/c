571ea715c49b5bf890b1f96ea5b25610107534dd
marcusDenslow <marcus.allen.denslow@gmail.com>
2025-02-19 19:12:22 +0100

    first commit

---
+++ first.c
@@ -0,0 +1,24 @@
+#include "raylib.h"
+
+int main(void){
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+
+
+  InitWindow(screenWidth, screenHeight, "raylib example");
+
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()){
+    BeginDrawing();
+
+      ClearBackground(RAYWHITE);
+      
+      DrawText("this is a window", 190, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
